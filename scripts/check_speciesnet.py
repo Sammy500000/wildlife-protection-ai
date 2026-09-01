2bedcd2628e908b327c3ab66e544f8a103dc5583
Sammy500000 <pratyushg2006@gmail.com>
2026-09-02 01:59:29 +0530

    test: add SpeciesNet environment check

---
+++ scripts/check_speciesnet.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ml.species.speciesnet import SpeciesNetAdapter
+
+
+if __name__ == "__main__":
+    SpeciesNetAdapter.check_installation()
+    print("SPECIESNET_OK")
